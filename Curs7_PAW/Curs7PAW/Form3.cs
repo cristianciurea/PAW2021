5162609d4c1191b968dcf95898ec84fce376f5b3
CRISTIAN CIUREA <cristianciurea@gmail.com>
2021-04-05 18:52:13 +0300

    update

---
+++ Curs7_PAW/Curs7PAW/Form3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Curs7PAW
+{
+    public partial class Form3 : Form
+    {
+        public Form3(ArrayList lista)
+        {
+            InitializeComponent();
+            foreach (Abonat a in lista)
+                textBox1.Text += a.ToString() + Environment.NewLine;
+        }
+    }
+}
