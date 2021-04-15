eaf3c640cba0edbc517754a57a1ff02b56c005ad
CRISTIAN CIUREA <cristianciurea@gmail.com>
2021-04-15 21:54:26 +0300

    update

---
+++ Sem8_PAW/Sem8PAW_1046/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Sem8PAW_1046
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.desenareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dreptunghiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elipsaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.incarcaDateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.schimbaCuloareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schimbaFontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.printPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.desenareToolStripMenuItem,
+            this.incarcaDateToolStripMenuItem,
+            this.printPreviewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1033, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // desenareToolStripMenuItem
+            // 
+            this.desenareToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dreptunghiToolStripMenuItem,
+            this.elipsaToolStripMenuItem});
+            this.desenareToolStripMenuItem.Name = "desenareToolStripMenuItem";
+            this.desenareToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.desenareToolStripMenuItem.Text = "Desenare";
+            // 
+            // dreptunghiToolStripMenuItem
+            // 
+            this.dreptunghiToolStripMenuItem.Name = "dreptunghiToolStripMenuItem";
+            this.dreptunghiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dreptunghiToolStripMenuItem.Text = "Dreptunghi";
+            this.dreptunghiToolStripMenuItem.Click += new System.EventHandler(this.dreptunghiToolStripMenuItem_Click);
+            // 
+            // elipsaToolStripMenuItem
+            // 
+            this.elipsaToolStripMenuItem.Name = "elipsaToolStripMenuItem";
+            this.elipsaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.elipsaToolStripMenuItem.Text = "Elipsa";
+            this.elipsaToolStripMenuItem.Click += new System.EventHandler(this.elipsaToolStripMenuItem_Click);
+            // 
+            // incarcaDateToolStripMenuItem
+            // 
+            this.incarcaDateToolStripMenuItem.Name = "incarcaDateToolStripMenuItem";
+            this.incarcaDateToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.incarcaDateToolStripMenuItem.Text = "Incarca date";
+            this.incarcaDateToolStripMenuItem.Click += new System.EventHandler(this.incarcaDateToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.schimbaCuloareToolStripMenuItem,
+            this.schimbaFontToolStripMenuItem,
+            this.salvareToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(163, 70);
+            // 
+            // schimbaCuloareToolStripMenuItem
+            // 
+            this.schimbaCuloareToolStripMenuItem.Name = "schimbaCuloareToolStripMenuItem";
+            this.schimbaCuloareToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.schimbaCuloareToolStripMenuItem.Text = "Schimba culoare";
+            this.schimbaCuloareToolStripMenuItem.Click += new System.EventHandler(this.schimbaCuloareToolStripMenuItem_Click);
+            // 
+            // schimbaFontToolStripMenuItem
+            // 
+            this.schimbaFontToolStripMenuItem.Name = "schimbaFontToolStripMenuItem";
+            this.schimbaFontToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.schimbaFontToolStripMenuItem.Text = "Schimba font";
+            this.schimbaFontToolStripMenuItem.Click += new System.EventHandler(this.schimbaFontToolStripMenuItem_Click);
+            // 
+            // salvareToolStripMenuItem
+            // 
+            this.salvareToolStripMenuItem.Name = "salvareToolStripMenuItem";
+            this.salvareToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salvareToolStripMenuItem.Text = "Salvare";
+            this.salvareToolStripMenuItem.Click += new System.EventHandler(this.salvareToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.ContextMenuStrip = this.contextMenuStrip1;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1033, 483);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // printPreviewToolStripMenuItem
+            // 
+            this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
+            this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.printPreviewToolStripMenuItem.Text = "Print Preview";
+            this.printPreviewToolStripMenuItem.Click += new System.EventHandler(this.printPreviewToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1033, 507);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem desenareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dreptunghiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elipsaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem incarcaDateToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem schimbaCuloareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schimbaFontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvareToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem printPreviewToolStripMenuItem;
+    }
+}
+
---
+++ Sem8_PAW/Sem8PAW_1048/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace Sem8PAW_1048
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+
+        double[] vect = new double[20];
+        int nrElem = 0;
+        bool vb = false;
+
+        const int marg = 10;
+
+        Color culoare = Color.Blue;
+        Font font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Bold);
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics(); //1
+            /*g = Graphics.FromHwnd(this.Handle); //2
+            Bitmap img = new Bitmap(this.Width, this.Height);
+            g = Graphics.FromImage(img); //3*/
+        }
+
+        private void dreptunghiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Red, 3);
+            Rectangle rec = new Rectangle(40, 40, 300, 300);
+            g.DrawRectangle(pen, rec);
+        }
+
+        private void elipsaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Red, 3);
+            Rectangle rec = new Rectangle(40, 40, 300, 300);
+            g.DrawEllipse(pen, rec);
+        }
+
+        private void incarcaDateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("fisier.txt");
+            string linie = null;
+            while((linie = sr.ReadLine())!=null)
+            {
+                vect[nrElem] = Convert.ToDouble(linie);
+                nrElem++;
+                vb = true;
+            }
+            sr.Close();
+            MessageBox.Show("Date incarcate!");
+            //this.Invalidate();
+            panel1.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if(vb==true)
+            {
+                Graphics gr = e.Graphics; //4
+                Rectangle rec = new Rectangle(this.ClientRectangle.X+marg, this.ClientRectangle.Y+4*marg,
+                    this.ClientRectangle.Width-2*marg, this.ClientRectangle.Height-5*marg);
+                Pen pen = new Pen(Color.Red, 3);
+                gr.DrawRectangle(pen, rec);
+
+                double latime = rec.Width / nrElem / 3;
+                double distanta = (rec.Width - nrElem * latime) / (nrElem + 1);
+                double vMax = vect.Max();
+
+                Brush brush = new SolidBrush(culoare);
+
+                Rectangle[] recs = new Rectangle[nrElem];
+                for(int i=0;i<nrElem;i++)
+                {
+                    recs[i] = new Rectangle((int)(rec.Location.X + (i + 1) * distanta + i * latime),
+                        (int)(rec.Location.Y + rec.Height - vect[i] / vMax * rec.Height),
+                        (int)latime,
+                        (int)(vect[i] / vMax * rec.Height));
+                    //gr.FillRectangle(brush, recs[i]);
+                    //gr.FillEllipse(brush, recs[i]);
+                    gr.DrawString(vect[i].ToString(), font, brush,
+                        new Point(recs[i].Location.X, recs[i].Location.Y - font.Height));
+                }
+                gr.FillRectangles(brush, recs);
+
+                for (int i = 0; i < nrElem - 1; i++)
+                    gr.DrawLine(pen, new Point((int)(recs[i].Location.X + latime/2), recs[i].Location.Y),
+                        new Point((int)(recs[i+1].Location.X + latime / 2), recs[i+1].Location.Y));
+            }
+        }
+
+        private void schimbaCuloareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+                culoare = dlg.Color;
+            //this.Invalidate();
+            panel1.Invalidate();
+        }
+
+        private void schimbaFontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog dlg = new FontDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+                font = dlg.Font;
+            //this.Invalidate();
+            panel1.Invalidate();
+        }
+
+        private void save(Control c, string denumire)
+        {
+            Bitmap img = new Bitmap(c.Width, c.Height);
+            c.DrawToBitmap(img, new Rectangle(c.ClientRectangle.X, c.ClientRectangle.Y,
+                c.ClientRectangle.Width, c.ClientRectangle.Height));
+            img.Save(denumire);
+            img.Dispose();
+        }
+
+        private void salvareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //save(this, "grafic.bmp");
+            save(panel1, "grafic.bmp");
+            MessageBox.Show("Salvat!");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (vb == true)
+            {
+                Graphics gr = e.Graphics; //4
+                Rectangle rec = new Rectangle(panel1.ClientRectangle.X + marg, panel1.ClientRectangle.Y + 4 * marg,
+                    panel1.ClientRectangle.Width - 2 * marg, panel1.ClientRectangle.Height - 5 * marg);
+                Pen pen = new Pen(Color.Red, 3);
+                gr.DrawRectangle(pen, rec);
+
+                double latime = rec.Width / nrElem / 3;
+                double distanta = (rec.Width - nrElem * latime) / (nrElem + 1);
+                double vMax = vect.Max();
+
+                Brush brush = new SolidBrush(culoare);
+
+                Rectangle[] recs = new Rectangle[nrElem];
+                for (int i = 0; i < nrElem; i++)
+                {
+                    recs[i] = new Rectangle((int)(rec.Location.X + (i + 1) * distanta + i * latime),
+                        (int)(rec.Location.Y + rec.Height - vect[i] / vMax * rec.Height),
+                        (int)latime,
+                        (int)(vect[i] / vMax * rec.Height));
+                    //gr.FillRectangle(brush, recs[i]);
+                    //gr.FillEllipse(brush, recs[i]);
+                    gr.DrawString(vect[i].ToString(), font, brush,
+                        new Point(recs[i].Location.X, recs[i].Location.Y - font.Height));
+                }
+                gr.FillRectangles(brush, recs);
+
+                for (int i = 0; i < nrElem - 1; i++)
+                    gr.DrawLine(pen, new Point((int)(recs[i].Location.X + latime / 2), recs[i].Location.Y),
+                        new Point((int)(recs[i + 1].Location.X + latime / 2), recs[i + 1].Location.Y));
+            }
+        }
+
+        private void pdPrint(object sender, PrintPageEventArgs e)
+        {
+            if (vb == true)
+            {
+                Graphics gr = e.Graphics; //4
+                Brush brush = new SolidBrush(culoare);
+                gr.DrawString("Lista studentilor bursieri: ", font, brush,
+                    new Point(e.PageBounds.X + 100, e.PageBounds.Y + 50 - font.Height));
+
+                gr.DrawString("Gigel = media 10 ", font, brush,
+                   new Point(e.PageBounds.X + 100, e.PageBounds.Y + 150 - font.Height));
+
+                /*Rectangle rec = new Rectangle(e.PageBounds.X + marg, e.PageBounds.Y + 4 * marg,
+                   e.PageBounds.Width - 2 * marg, e.PageBounds.Height - 5 * marg);
+                Pen pen = new Pen(Color.Red, 3);
+                gr.DrawRectangle(pen, rec);
+
+                double latime = rec.Width / nrElem / 3;
+                double distanta = (rec.Width - nrElem * latime) / (nrElem + 1);
+                double vMax = vect.Max();
+
+                Brush brush = new SolidBrush(culoare);
+
+                Rectangle[] recs = new Rectangle[nrElem];
+                for (int i = 0; i < nrElem; i++)
+                {
+                    recs[i] = new Rectangle((int)(rec.Location.X + (i + 1) * distanta + i * latime),
+                        (int)(rec.Location.Y + rec.Height - vect[i] / vMax * rec.Height),
+                        (int)latime,
+                        (int)(vect[i] / vMax * rec.Height));
+                    //gr.FillRectangle(brush, recs[i]);
+                    //gr.FillEllipse(brush, recs[i]);
+                    gr.DrawString(vect[i].ToString(), font, brush,
+                        new Point(recs[i].Location.X, recs[i].Location.Y - font.Height));
+                }
+                gr.FillRectangles(brush, recs);
+
+                for (int i = 0; i < nrElem - 1; i++)
+                    gr.DrawLine(pen, new Point((int)(recs[i].Location.X + latime / 2), recs[i].Location.Y),
+                        new Point((int)(recs[i + 1].Location.X + latime / 2), recs[i + 1].Location.Y));*/
+            }
+        }
+
+        private void printPreviewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(pdPrint);
+            PrintPreviewDialog dlg = new PrintPreviewDialog();
+            dlg.Document = pd;
+            dlg.ShowDialog();
+        }
+    }
+}
